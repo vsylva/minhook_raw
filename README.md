75d52126d1618d7c0b7b0298a95ce9bb33bbc3f3
vSylva <58687743+vSylva@users.noreply.github.com>
2024-05-31 15:48:19 +0800

    Update

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-[![Crates.io Version](https://img.shields.io/crates/v/minhook_raw?style=for-the-badge)](https://crates.io/crates/minhook_raw)
-
-[build.rs](https://github.com/veeenu/hudhook/blob/main/build.rs)
-
 ```rust
 use minhook_raw::*;
 // use minhook_raw::sys::*;
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-#![doc = "[![Crates.io Version](https://img.shields.io/crates/v/minhook_raw?style=for-the-badge)](https://crates.io/crates/minhook_raw)
-
-[build.rs](https://github.com/veeenu/hudhook/blob/main/build.rs)
-
-```rust
+#![doc = "```rust
 use minhook_raw::*;
 // use minhook_raw::sys::*;
 ```"]
